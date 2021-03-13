daeb7c54d42ab61cb0aa3d36da404b9f55f69424
Jade Kang <jkang.se@gmail.com>
2021-03-12 22:37:33 -0800

    fixed the index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 const Index = () => {
-  <div>
-    <p>Building a simple shopify app using React + Next.js</p>
-  </div>;
+  return (
+    <div>
+      <p>Building a simple shopify app using React + Next.js</p>
+    </div>
+  );
 };
 
 export default Index;
